9c8b815461d26235461cf78672080464bc2f6c95
garlife <garlife@gmail.com>
2022-01-15 13:31:57 +0300

    error_try&catch

--- src/ErrorBoundary.js
+++ src/ErrorBoundary.js
@@ -13,8 +13,26 @@ export default class ErrorBoundary extends PureComponent {
     return { hasError: true, error };
   }
 
+  componentDidCatch(error, info){
+  try {
+
+    logComponentStatck(info.componentStack);
+	}
+catch (event){
+console.log('error boundary: ', event.massage;
+}
+  }
+
   render() {
+    const { hasError } = this.state;
     const { children } = this.props;
-    return this.props.children;
+    if (hasError) {
+      return <h1>Error: {error}</h1>;
+    }
+    const { children } = this.props;
+    return children;
+  }
+  componentStack(log){
+    console.log('log: ', new Date.toLocaleTimeString(), log)
   }
 }
