b73b0342ae5919f577d46c08df12670a89c45633
garlife <garlife@gmail.com>
2022-01-15 12:24:51 +0300

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,11 +1,67 @@
-import React from "react";
-import "./style.css";
+import React, { Component } from 'react';
 
-export default function App() {
-  return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
-  );
+class Clock extends Component {
+  state = {
+    date: new Date(),
+  };
+
+  timerId;
+
+  constructor(props) {
+    super(props);
+    this.list = React.createRef();
+  }
+
+  componentDidMount() {
+    this.timerId = setInterval(() => this.tick(), 1000);
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    if (prevProps.list.length < this.props.list.length) {
+      const list = this.listRef.current;
+      return list.scrollHeight - list.scrollTop;
+    }
+    return null;
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    // if (this.props.userId !== prevProps.userId) {
+    // this.setState((pState) => ({ date: new Date() }));
+    if (snapshot !== null) {
+      const list = this.listRef.current;
+      list.scrollTop = list.scrollHeight - snapshot;
+    }
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.timerId);
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Lesson 5. DateTick</h1>
+        <p>{this.state.date.toLocaleTimeString()}</p>
+        <div ref={this.listRef}>Content</div>
+      </>
+    );
+  }
+
+  tick() {
+    this.setState((prevState) => ({ date: new Date() }));
+    //this.state = {date: new Date()};  Don't do like this!
+    console.log(this.state.date.toLocaleTimeString());
+  }
+}
+
+export default class App extends Component {
+  render() {
+    return (
+      <>
+        {false ? <Clock /> : ''}
+        <Clock />
+        {false ? <Clock /> : ''}
+      </>
+    );
+  }
 }
